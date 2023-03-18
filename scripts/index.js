c4ca632cbbd6f157c6095490716706db0cc2bccd
Artem Riabykh <riabykhartem@yandex.ru>
2023-03-18 17:13:25 -0500

    2nd attempt

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,32 @@
 // DOM элементы
-const popupElement = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = popupElement.querySelector('.form__close-button');
-const nameInput = popupElement.querySelector('.form__input-name');
-const descriptionInput = popupElement.querySelector('.form__input-description');
-const saveButton = popupElement.querySelector('.form__save-button');
+const popupElement = document.querySelector(".popup");
+const popupOpenButton = document.querySelector(".profile__edit-button");
+const popupCloseButton = popupElement.querySelector(".popup__close-button");
+const nameInput = popupElement.querySelector(".form__input_name");
+const descriptionInput = popupElement.querySelector(".form__input_description");
+const saveButton = popupElement.querySelector(".form__save-button");
 const nameElement = document.querySelector(".profile__name");
-const descriptionElement = document.querySelector('.profile__description')
+const descriptionElement = document.querySelector(".profile__description");
+let formElement = document.querySelector(".form");
 
 // функции открытия закрытия попапа попапа
 const openPopup = function () {
-    popupElement.classList.add("popup_is-opened");
+  nameInput.value = nameElement.textContent;
+  descriptionInput.value = descriptionElement.textContent;
+  popupElement.classList.add("popup_opened");
 };
 const closePopup = function () {
-    popupElement.classList.remove("popup_is-opened");
-}
+  popupElement.classList.remove("popup_opened");
+};
 popupOpenButton.addEventListener("click", openPopup);
 popupCloseButton.addEventListener("click", closePopup);
 
 // фукция изменения текстового содержания
-let formElement = document.querySelector('.form')
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    nameElement.textContent = nameInput.value;
-    descriptionElement.textContent = descriptionInput.value;
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  nameElement.textContent = nameInput.value;
+  descriptionElement.textContent = descriptionInput.value;
+  closePopup();
 }
-formElement.addEventListener('submit', handleFormSubmit)
+formElement.addEventListener("submit", handleFormSubmit);
